161f7748f01411a1b4d956b93d77f765a39bd7ad
TenzukiSushi <ppatrickalmeidda27@gmail.com>
2020-11-18 13:15:52 -0300

    Refazendo o jogo
    Movimentação e animação do player refeito, rotação do player refeitos

---
+++ Carnificina Mundial/Assets/Scripts/Player/PlayerHands.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerHands : MonoBehaviour
+{
+    [SerializeField] private GameObject hands;
+
+    private Rigidbody2D rb;
+    private Vector2 mousePos;
+    private SpriteRenderer sprite;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    private void Update() => mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    private void FixedUpdate() => GunRotating();
+
+    private void GunRotating()
+    {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;    //Faz os calculos necessarios para descobrir a rotação
+        hands.transform.rotation = Quaternion.Euler(0, 0, angle);           //Aplica a rotação de acordo com a posição do mouse
+      
+        if (angle > 90 || angle < -90)
+        {
+            sprite.flipX = true;
+            hands.GetComponent<SpriteRenderer>().flipY = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+            hands.GetComponent<SpriteRenderer>().flipY = false;
+        }
+    }
+}
--- Carnificina Mundial/Assets/Scripts/Player/PlayerMovement.cs
+++ Carnificina Mundial/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,95 +1,37 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
 
-    public int health;
-    public bool invulnerable = false;
-
     private float currentSpeed;
 
-    private Rigidbody2D rb;
-    private Animator anim;
-    private Animator lifeBar;
-    private SpriteRenderer sprite;
-
     private Vector2 axis;
+    private Animator anim;
+    private Rigidbody2D rb;
 
+    private void FixedUpdate() => rb.velocity = axis * currentSpeed;
+
+    //Pega o componente dos tipos
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-        sprite = GetComponent<SpriteRenderer>();
-        lifeBar = GameObject.Find("LifeBar").GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
         Animations();
-        lifeBar.SetInteger("Health", health);
-
         axis = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        if (Input.GetKey(KeyCode.LeftShift)) currentSpeed = runSpeed;
-        else currentSpeed = walkSpeed;
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = axis * currentSpeed;   
-    }
+        if(Input.GetKey(KeyCode.LeftShift)) currentSpeed = runSpeed;    //Dá ao currentSpeed a velocidade de correr
+        else currentSpeed = walkSpeed;                                  //Dá ao currentSpeed a velocidade de andar
+    }   
 
     private void Animations()
     {
-        bool canAnimWalk;
-
-        if (axis.x != 0) canAnimWalk = true;
-
-        else if (axis.y != 0) canAnimWalk = true;
-        else if (axis.x != 0 || axis.y != 0) canAnimWalk = true;
-        else canAnimWalk = false;
-
-        anim.SetBool("Moving", canAnimWalk);
-    }
-
-    IEnumerator Damage()
-    {
-        for(float i = 0; i < 1; i += 0.1f)
-        {
-            sprite.enabled = false;
-            yield return new WaitForSeconds(0.1f);
-            sprite.enabled = true;
-            yield return new WaitForSeconds(0.1f);
-        }
-
-        invulnerable = false;
-    }
-
-    IEnumerator Die()
-    {
-        currentSpeed = 0;
-        walkSpeed = 0;
-        runSpeed = 0;
-        for (float i = 0; i < 1; i += 0.1f)
-        {
-            sprite.enabled = false;
-            yield return new WaitForSeconds(0.1f);
-            sprite.enabled = true;
-            yield return new WaitForSeconds(0.1f);
-        }
-        SceneManager.LoadScene("Menu");
-    }
-
-    public void DamagePlayer()
-    {
-        invulnerable = true;
-        health--;
-        StartCoroutine(Damage());
-
-        if(health <= 0) StartCoroutine(Die());
+        if (axis != new Vector2(0, 0)) anim.SetBool("Moving", true);    //Ativa animação de corrida
+        else anim.SetBool("Moving", false);                             //Desativa a animação de corrida
     }
 }
